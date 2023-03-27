6101a4b9359a9185883371203bb09235b2f0c118
Tksmith-guru <crunchyk837@gmail.com>
2023-03-27 14:13:56 -0700

    Quick fix main.h

--- main.h
+++ main.h
@@ -19,26 +19,25 @@
 #define S_SHORT 1
 
 /**
- * struct fmt - struct op
+ * struct fmt - Struct op
  *
- * @fmt: the format
- * @fn: The function associated
+ * @fmt: The format.
+ * @fn: The function associated.
  */
-struct fmt 
+struct fmt
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
 };
 
 
-
 /**
- * typedef struct fmt fmt_t - struct op
+ * typedef struct fmt fmt_t - Struct op
  *
- * @fmt: The format
- * @fm_t: The function associated
+ * @fmt: The format.
+ * @fm_t: The function associated.
  */
-typedef struct fmt_t;
+typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
